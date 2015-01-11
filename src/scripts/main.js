410abf165e1c91fab2838d21b04b1b93acd86b19
jarndt <jtcols@gmail.com>
2015-01-11 11:58:16 -0500

    Basic project scaffolding

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,21 +5,21 @@
  */
 
 //Entry point
-$( document ).ready(function() {
+$(document).ready(function () {
     var boxManager;
 
     /**
      * Box manager object
      *
      */
-    boxManager = (function() {
+    boxManager = (function () {
 
         var dataModel = [],
             boxIndex = 1,
             colorIndex = 1,
-            source   = $("#box-template").html(),
+            source = $("#box-template").html(),
             boxTemplate = Handlebars.compile(source),
-            containerSource =  $("#group-container-template").html(),
+            containerSource = $("#group-container-template").html(),
             groupContainerTemplate = Handlebars.compile(containerSource);
 
         return {
@@ -27,7 +27,7 @@ $( document ).ready(function() {
             /**
              * Initialization Function
              */
-            init: function() {
+            init: function () {
                 //initialize the page with one box.
                 this.addBox();
             },
@@ -38,33 +38,49 @@ $( document ).ready(function() {
              *
              *
              * @param e - The event that initiated the add, the new element will be inserted after its target.
+             * @param redrawPage - boolean telling us if we can just insert this new box at the end or if we really
+             *        need to redraw the page
              */
-            addBox: function(e) {
+            addBox: function (e, redrawPage) {
                 var dataObj,
+                    currIndex,
                     boxId = this.getNextId();
 
-                //create the data object for use by Handelbars template
+                //create the data object for use by Handlebars template
                 dataObj = {
                     boxId: boxId,
-                    leftNumber: boxId - 1,
-                    rightNumber: boxId + 1,
-                    backgroundStyle: null
+                    leftNumber: function(){
+                        return this.arrayPosition;
+                    },
+                    rightNumber: function(){
+                        return this.arrayPosition + 2;
+                    },
+                    backgroundStyle: null,
+                    arrayPosition: dataModel.length
                 };
 
                 //add styles
                 dataObj = this.applyStyles(dataObj);
 
-                //add this to the global dataModel for persistence
-                dataModel.push(dataObj);
-
                 //place the element into the dom
-                this.insertBoxIntoDom(dataObj);
+                if (redrawPage) {
+                    currIndex = $(e.currentTarget).find(".arrayPosition")[0].value;
+
+
+                    dataModel.splice(currIndex+1, 0, dataObj);
+                    this.renderAll();
+                } else {
+                    //add this to the global dataModel for persistence
+                    dataModel.push(dataObj);
+                    this.insertBoxIntoDom(dataObj);
+                }
+
             },
 
 
             /**
-             *For performance reasons we do not want to re-render the entire data model, that would just be wasteful.
-             * We can be a bit smart and just insert a box where it needs to go.
+             *For performance reasons we do not want to re-render the entire data model, if we can help usthat would just be wasteful.
+             * We can be a bit smart and just insert a box where it needs to go if the last element was clicked.
              *
              * There is a good bit of logic needed  because of the need to add another group container every 6 boxes
              *
@@ -72,7 +88,7 @@ $( document ).ready(function() {
              *
              * @param dataObj
              */
-            insertBoxIntoDom: function(dataObj) {
+            insertBoxIntoDom: function (dataObj) {
                 var insertElem,
                     containerElement,
                     groupElement,
@@ -80,10 +96,11 @@ $( document ).ready(function() {
 
                 //create the element to insert by applying data to the
                 //pre-compiled handlebars template, then turn it into a JQuery object for your programming pleasure
+
                 insertElem = $(boxTemplate(dataObj));
 
                 //initial state and we need to insert a group-container
-                if($(".container-2 .group-container").length < 1){
+                if ($(".container-2 .group-container").length < 1) {
 
                     //group container
                     containerElement = $(groupContainerTemplate());
@@ -92,20 +109,29 @@ $( document ).ready(function() {
                     //insert first box
                     groupElement = $(".group-container:last-child");
                     groupElement.html(insertElem);
-                }else{
+                } else {
                     //We have a group container
                     groupElement = $(".group-container:last-child");
 
                     //is it time for a new group container?
-                    if(groupElement.children().length === 6){
+                    if (groupElement.children().length === 6) {
                         //New group container for sequencing
                         containerElement = $(groupContainerTemplate());
                         containerElement.insertAfter(groupElement);
                         containerElement.html(insertElem);
 
                     } else {
+                        var that = this;
+
                         //let's clean up the old event handler
-                        currLastElement.unbind( "click" );
+                        currLastElement.unbind("click");
+
+                        //we then need to add a new event handler
+                        //Add click event to 2nd to last child box
+                        currLastElement.click(function (e) {
+                            that.addBox(e, true);
+                            //window.alert("Hey Hey Hey")
+                        });
 
                         //insert the element into the DOM at the proper location
                         insertElem.insertAfter(currLastElement);
@@ -118,24 +144,46 @@ $( document ).ready(function() {
 
             },
 
+            /**
+             * We could be lazy and  just call this after every insert and be done with it. This would be less than
+             * optimal in any mobile environment. We only want to call this when we are forced to redraw the entire
+             * page.
+             */
 
-            render: function(){
-                var i;
+            renderAll: function () {
+                var i = 0,
+                    currentBox,
+                    that = this;
 
-                for(;i< dataModel.length;i++){
-                    this.insertBoxIntoDom(dataModel);
+                //out with the old
+                this.clearDisplay();
+
+                //reset the color Index for redisplay
+                colorIndex = 1
+
+                //loop through dataModel and rebuild display. We will need reset some of the data
+                for (; i < dataModel.length; i++) {
+                    currentBox = dataModel[i];
+                    that.applyStyles(currentBox);
+
+
+                    this.insertBoxIntoDom(currentBox);
                 }
 
             },
 
 
+            clearDisplay: function(){
+                $( ".container-2" ).empty();
+            },
+
             /**
              * The element needs styles added for width and color
              * @param dataObj
              */
-            applyStyles : function(dataObj){
+            applyStyles: function (dataObj) {
                 var style;
-                switch(colorIndex){
+                switch (colorIndex) {
                     case 2:
                         style = "red";
                         colorIndex++;
@@ -163,15 +211,15 @@ $( document ).ready(function() {
              *
              * @param element
              */
-            addBoxEvents: function(element){
+            addBoxEvents: function (element) {
                 var that = this;
 
                 //Add click event to last child box
-                element.click(function(e){
+                element.click(function (e) {
                     that.addBox(e);
                 });
 
-                element.find(".delete-container").click(function(e){
+                element.find(".delete-container").click(function (e) {
                     that.removeBox(e);
                 });
             },
@@ -180,13 +228,17 @@ $( document ).ready(function() {
              *
              * @param e
              */
-            removeBox: function(e){
+            removeBox: function (e) {
 
-                var boxElem = $(e.currentTarget).closest(".kl-box");
-                boxElem.remove();
+                var boxElem = $(e.currentTarget).closest(".kl-box"),
+                    currIndex = boxElem.find(".arrayPosition")[0].value;
 
+                //update the model removing the selected element
+                dataModel.splice(currIndex, 1);
+
+                this.renderAll();
                 //add events to new last box
-                this.addBoxEvents($(".group-container:last-child .kl-box:last-child"));
+                //this.addBoxEvents($(".group-container:last-child .kl-box:last-child"));
 
                 //todo add events to new last box
                 e.stopPropagation();
@@ -195,16 +247,16 @@ $( document ).ready(function() {
             /**
              * grab the next id
              */
-            getNextId: function(){
+            getNextId: function () {
                 //todo add logic to handle setting boxIndex when the page is reopened.
-                return(boxIndex++);
+                return (boxIndex++);
             },
 
 
             /**
              *
              */
-            persist: function(){
+            persist: function () {
 
             }
         }
